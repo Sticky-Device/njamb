a5e88ca5dec1db87c717516b1e0cfe4123faf05a
todoric-nemanja <todoric.nemanja@gmail.com>
2017-01-18 18:28:25 +0100

    std::mt19937 is expensive - make sure to reuse it.

--- dicehand.cpp
+++ dicehand.cpp
@@ -4,7 +4,7 @@
 
 static int generateRandomDiceInteger()
 {
-    std::mt19937 rng;
+    static std::mt19937 rng;
     rng.seed(std::random_device()());
     std::uniform_int_distribution<std::mt19937::result_type> dist6(1,6);
     return dist6(rng);
